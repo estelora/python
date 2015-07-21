4c080f7faf58673980efcb23c00b9ef85fae2df5
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-21 19:06:33 -0400

    drawing octagons

--- turtle.py
+++ turtle.py
@@ -8,14 +8,25 @@ from turtle import *
 
 shape('turtle')
 color('#31B404')
-forward(100)
-right(90)
-forward(100)
-right(90)
-forward(100)
-right(90)
-forward(100)
-right(90)
+
+# draw a square
+
+for i in range(4):
+  forward(100)
+  right(90)
+
+
+# draw an octagon with alternation colors
+
+  for i in range(8):
+  
+    if i % 2 == 0:
+      color('maroon')
+    else:
+      color('firebrick')
+      
+    forward(70)
+    right(45)
 
 
 
