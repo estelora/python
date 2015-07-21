622a380c8503706e95ec7fd9153f18a1676a45b5
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-21 18:56:32 -0400

    turtle is marching in a square now

--- turtle.py
+++ turtle.py
@@ -3,8 +3,20 @@
 
 from turtle import *
 
+
+from turtle import *
+
 shape('turtle')
+color('#31B404')
 forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
 
 
 
