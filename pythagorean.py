21ac92842f65e700f018c6c8215ee58ef47cfd5e
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-21 18:30:14 -0400

    add float

--- pythagorean.py
+++ pythagorean.py
@@ -1,6 +1,6 @@
-a = int(input("Enter first side of the right triangle:"))
-b= int(input("Enter the second side of the right triangle:"))
+a = float(input("Enter first side of the right triangle:"))
+b = float(input("Enter the second side of the right triangle:"))
 
 c = (a**2 + b**2)**0.5
 
-print(c)
+print("Length of Hypotenuse: " + c)
